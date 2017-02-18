4ac2f979d021c6e9a1552dab03f551be05c0c2c3
Danny <vonnenaut@users.noreply.github.com>
2017-02-19 00:46:35 +0700

    Update realmofreckoning.py

--- realmofreckoning.py
+++ realmofreckoning.py
@@ -152,7 +152,6 @@ class Map(object):
 	def teleport(self, coords):
 		#loc = self.retrieve_Location(coords)
 		player.set_coords(coords)
-		print "Player coords: ",player.get_coords()
 		self.narrative(player.get_coords())
 
 	def narrative(self, area):
@@ -163,7 +162,7 @@ class Map(object):
 
 		narratives = {(0,0): ["\n     You are standing in the same field in which you first awoke\nwithout any memory of how you got here.  Which way?\n"],
 					  (-1,0): ["\nAs you proceed west, you come upon signs of a battle, \nincluding two bodies lying face-down at the edge of a wood\n near the field in which you awoke.  There are no signs of\n life.  Do you approach the bodies? (y/n)", self.encounter],
-					  (0,1): ["\n     [Missing narrative]"],
+					  (0,1): ["\n     You walk for some time to finally arrive at an old, \napparently abandoned fortress which has crumbled with time.  \nThere is nothing here but ruin.  As you approach an outcrop of rock, you realize there is a narrow passage leading down into the ground, perhaps an old cellar entrance.  Do you enter? (y/n)", self.encounter],
 					  (0,2): ["\n     As you head north around the abandoned and crumbling\nfortress you see a valley spread out before you.  In the\ndistance to the north is a village with wafts of smoke being\ncarried off by the breeze trailing over the scene like\nthe twisted tails of many kites.  To the west gently\nsloping foothills transition into distant blue mountains\nand to the east, a vast forest conspires to block out all \nsurface detail."]}
 
 		if player.newplayer == True:
@@ -191,6 +190,11 @@ class Map(object):
 		# call keydown method of player instance of Character class to handle user input
 		self.keydown(user_input)
 
+	def next_choice(choice_key):
+		""" increments choice_key's 3rd number to proceed a deeper level in the branching choices for any location """
+		choice_key[2] += 1
+		encounter(choice_key)
+
 	def encounter(self, choice_key):
 		# a third number has been added to the tuple key to represent level of depth (decision/choice 1: outcome, decision/choice 2/outcome, etc.)  The Value of each of these keys goes in the form:  ['user-input choice', print narrative, command(s) to execute]
 		
@@ -198,17 +202,21 @@ class Map(object):
 		input = raw_input(prompt).lower()
 
 		outcomes = { (-1,0,1): 
-								{'y': ["\nAs you approach one of the bodies closely, you realize\n that he is feigning death when he rolls quickly and \nsinks a blade into your neck.  Your life fades slowly.", player.die], 'n': ["\nWell, daylight's a-wasting.  Where to now?"] } }
+								{'y': ["\nAs you approach one of the bodies closely, you realize\n that he is feigning death when he rolls quickly and \nsinks a blade into your neck.  Your life fades slowly.", player.die], 'n': ["\nWell, daylight's a-wasting.  Where to now?"]}, 
+					 (0,1,1):
+					 			{'y': ["\nYou steal yourself and proceed down the cracked and disintegrating steps only to find a locked wooden door.  Do you attempt to force the lock? (y/n)", self.next_choice], 'n': ["\nWhere to now?"]},
+					 (0,1,2):   {'y': ["\nYou put all your force into it as you drive your shoulder into the door.  It gives way in a sudden explosion of splinters, dust and debris.  It's too dark to proceed safely without a lantern."], 'n': ["\nWhere to now?"]} }
 		if choice_key in outcomes:
 			print outcomes[choice_key][input][0]
 			if len(outcomes[choice_key][input]) > 1:
 				for i in range (1,len(outcomes[choice_key][input])):
 					outcomes[choice_key][input][i]()
 		else:
-			for k in outcomes[choice_key]:
-				print "Please type "
-				for i in outcomes[choice_key]:
-					print "",outcomes[choice_key].key()
+			# for k in outcomes[choice_key]:
+				# print "Please type "
+				# for i in outcomes[choice_key]:
+					# print "",outcomes[choice_key].key()
+			pass
 
 	def keydown(self, user_input):
 		""" Handles user input """
@@ -393,7 +401,7 @@ class Character(object):
 	def check_time(self):
 		""" handles keeping track of in-game time, which is planned to affect interactions in the game """
 		pass
-		
+
 	def die(self):
 		"""	this function ends the program """
 		message = prRed("\nGame Over.\n")
