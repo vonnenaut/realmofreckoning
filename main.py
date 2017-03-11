d791a72240649498f20ea70ecae2f3e8d69686f3
Danny <vonnenaut@users.noreply.github.com>
2017-03-11 12:46:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,6 @@
 # by
 # Daniel Ashcom
 # GNU GPL v 3.0 but I want my free beer.
-# 
-#====================================================================
-# To-Do/Ideas:
-#-------------------------------------------------------------------
-# -- Continue with implementing more readable, concise ways of handling logic, replacing if-else with dictionaries (I think ... ?) --  See Coursera Fundamentals of Computing 2of7, wk 7b Programming Tips (02:30)
-# 
-# --Add more text narrative of areas to explore
-# --add item persistence on the ground when items are dropped
-# --Implement item usage (health potion, equipping weapons, etc.)
-# --Implement battle system
-# --Consider whether to modify this with Python libraries, for fun.
-# --Consider creating a visual version of this game once it has more written content
-# --Consider how to create randomly-generated areas with randomly-generated loot (<--  might be a good first step to implement.  See 'Design Patterns:  Elements of Reusable OO Software', p. 105)
-#
 #  ==========  End documentation  ==========
 ###
 
