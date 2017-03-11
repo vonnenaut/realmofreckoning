f96acfc6ab4fc8230c1f36e20059b223be21dc83
Danny <vonnenaut@users.noreply.github.com>
2017-03-11 13:36:46 +0700

    Update realm.py

--- realm.py
+++ realm.py
@@ -84,48 +84,46 @@ class Realm(object):
 		return self.MONSTER_LIST
 
 	def narrative(self, area):
-		""" Handles delivery of narrative text based on location as well as any choices available in any given location 
-			TO_DO:  repair alternate version which allows for encounters
-		"""
+		""" Handles delivery of narrative text based on location as well as any choices available in any given location """
 
 		key = tuple(area)
 
-		narratives = {(0,0): "You are standing in the same field in which you first awoke\nwithout any memory of how you got here.  Which way?\n",
-					 (-1,0): "\nAs you proceed west, you come upon signs of a battle, \nincluding two bodies lying face-down at the edge of a wood\n near the field in which you awoke.  There are no signs of\n life.  Do you approach the bodies? (y/n)",
-					  (0,1): "\n     You walk for some time to finally arrive at an old, \napparently abandoned, fortress which has crumbled with time.  \nThere is nothing here but ruin.  As you approach an outcrop of rock, you realize there is a narrow passage leading down into the ground, perhaps an old cellar entrance.  Do you enter? (y/n)",
-					  (0,2): "\n     As you head north around the abandoned and crumbling\nfortress you see a valley spread out before you.  In the\ndistance to the north is a village with wafts of smoke being\ncarried off by the breeze trailing over the scene like\nthe twisted tails of many kites.  To the west gently\nsloping foothills transition into distant blue mountains\nand to the east, a vast forest conspires to block out all \nsurface detail."
-					 }
-
-		# narratives = {(0,0): ["\n     You are standing in the same field in which you first awoke\nwithout any memory of how you got here.  Which way?\n"],
-					  # (-1,0): ["\nAs you proceed west, you come upon signs of a battle, \nincluding two bodies lying face-down at the edge of a wood\n near the field in which you awoke.  There are no signs of\n life.  Do you approach the bodies? (y/n)"],
-					  # (0,1): ["\n     You walk for some time to finally arrive at an old, \napparently abandoned, fortress which has crumbled with time.  \nThere is nothing here but ruin.  As you approach an outcrop of rock, you realize there is a narrow passage leading down into the ground, perhaps an old cellar entrance.  Do you enter? (y/n)"],
-					  # (0,2): ["\n     As you head north around the abandoned and crumbling\nfortress you see a valley spread out before you.  In the\ndistance to the north is a village with wafts of smoke being\ncarried off by the breeze trailing over the scene like\nthe twisted tails of many kites.  To the west gently\nsloping foothills transition into distant blue mountains\nand to the east, a vast forest conspires to block out all \nsurface detail."]}
+		narratives = {(0,0): ["\n     You are standing in the same field in which you first awoke\nwithout any memory of how you got here.  Which way?\n"],
+				  	 (-1,0): ["\nAs you proceed west, you come upon signs of a battle, \nincluding two bodies lying face-down at the edge of a wood\n near the field in which you awoke.  There are no signs of\n 	life.  Do you approach the bodies? (y/n)", self.encounter],
+				  	 (0,1): ["\n     You walk for some time to finally arrive at an old, \napparently abandoned fortress which has crumbled with time.  \nThere is nothing here but ruin.  As you approach an outcrop of rock, you realize there is a narrow passage leading down into the ground, perhaps 	an old cellar entrance.  Do you enter? (y/n)", self.encounter],
+				  	 (0,2): ["\n     As you head north around the abandoned and crumbling\nfortress you see a valley spread out before you.  In the\ndistance to the north is a village with wafts of smoke being\ncarried off by the breeze trailing over the scene like\nthe twisted tails of many kites.  To the west gently\nsloping foothills transition into distant blue mountains\nand to the east, a 	vast forest conspires to block out all \nsurface detail."]}
 
 		if self.player.newplayer == True:
-			print "\n     You awaken to the distant sound of commotion to your west.  \nYou open your eyes and realize you are in a vulnerable spot\nin an open field.  You look west toward the direction of\nthe distant sounds and hear that it is now quiet.  To your \nnorth in the distance is a fortress.  To your east is a \nforest and to your south is a riverbank with a boat tied at \na pier.  \n\nWhich direction do you go? (Press 'h' for help)"
+			print "\n     You awaken to the distant sound of commotion to your west.  \nYou open your eyes and realize you are in a vulnerable spot\n in an open field.  You look west toward the direction of\n the distant sounds and hear that it is now quiet.  To your \nnorth in the distance is a fortress.  To your east is a \nforest and to your south is a riverbank with \aa boat tied at a pier.  \n\nWhich direction do you 	go? (Press 'h' for help)"
 			self.player.newplayer = False
 
 		elif key in narratives:
-			print narratives[key]
-
-
-			# if len(narratives[key]) > 1:  # if there are commands associated with this area, execute them
-				# for i in range(1,len(narratives[key])):
-					# temp_list = area[:]
-					# temp_list.append(i)
-					# choice_key = tuple(temp_list)
-					# narratives[key][i](choice_key)
- 		else:
+			print narratives[key][0]
+			if len(narratives[key]) > 1:  # if there are commands associated with this area, execute them
+				for i in range(1,len(narratives[key])):
+					temp_list = area[:]
+					temp_list.append(i)
+					choice_key = tuple(temp_list)
+					narratives[key][i](choice_key)
+		else:
 			prRed("Under construction.  Returning to the beginning.")
-			self.teleport([0,0])
+			self.player.teleport([0,0])
+			self.narrative(self.player.get_coords())
 	
 		# get user input
 		user_input = raw_input(self.PROMPT)
 		# get rid of any capitalized typing by the user
 		user_input.lower()
 
-		# call keydown to handle user input
+		# call keydown method of player instance of Character class to handle user input
 		self.keydown(user_input)
+
+	def next_choice(self, choice_key):
+		""" increments choice_key's 3rd number to proceed a deeper level in the branching choices for any location """
+		# cast choice_key back as a list so that it can be modified.  I'm pretty sure others would frown on this approach but it works and I have no idea of how else to create this functionality
+		choice_key = list(choice_key)
+		choice_key[2] += 1
+		self.encounter(choice_key)
 
 	def encounter(self, choice_key):
 		# a third number has been added to the tuple key to represent level of depth (decision/choice 1: outcome, decision/choice 2/outcome, etc.)  The Value of each of these keys goes in the form:  ['user-input choice', print narrative, command(s) to execute]
@@ -134,20 +132,22 @@ class Realm(object):
 		input = raw_input(self.PROMPT).lower()
 
 		# cast choice_key as a tuple so it can be used as a dictionary key
-		choice_key = tuple(choice_key)
+		outcomes_key = tuple(choice_key)
 
 		outcomes = { (-1,0,1): 
 								{'y': ["\nAs you approach one of the bodies closely, you realize\n that he is feigning death when he rolls quickly and \nsinks a blade into your neck.  Your life fades slowly.", self.die], 'n': ["\nWell, daylight's a-wasting.  Where to now?"]}, 
 					 (0,1,1):
 					 			{'y': ["\nYou steal yourself and proceed down the cracked and disintegrating steps only to find a locked wooden door.  Do you attempt to force the lock? (y/n)", self.next_choice], 'n': ["\nWhere to now?"]},
 					 (0,1,2):   {'y': ["\nYou put all your force into it as you drive your shoulder into the door.  It gives way in a sudden explosion of splinters, dust and debris.  It's too dark to proceed safely without a lantern."], 'n': ["\nWhere to now?"]} }
-		if choice_key in outcomes:
-			if len(outcomes[choice_key][input]) > 1:
-				for i in range (1,len(outcomes[choice_key][input])):
-					if outcomes[choice_key][input][i] == self.next_choice:
-						self.next_choice(choice_key)
+		if outcomes_key in outcomes:
+			if len(outcomes[outcomes_key][input]) > 1:
+				for i in range (1,len(outcomes[outcomes_key][input])):
+					if outcomes[outcomes_key][input][i] == self.next_choice:
+						self.next_choice(outcomes_key)
 					else:
-						outcomes[choice_key][input][i]()
+						outcomes[outcomes_key][input][i]()
+			else:
+				print outcomes[outcomes_key][input][0]
 		else:
 			print "Please type 'h' for help."
 
@@ -201,10 +201,6 @@ class Realm(object):
 			return self.locations[string]
 		except NameError:
 			print "Location does not exist."
-
-	def teleport(self, coords):
-		self.player.set_coords(coords)
-		self.narrative(self.player.get_coords())
 
 	def move(self, char, direction):
 		""" taking character instance and user input direction as parameters, handles player movement between Locations """
