bac6c434e24c50862ad76f2284299d750bc23d65
Dan Ashcom <danielashcom@gmail.com>
2025-01-05 08:35:55 -0700

    updated python2 syntax to python3 in testing code

--- realm_test.py
+++ realm_test.py
@@ -8,10 +8,10 @@ from character import Character
 def realm_test():
 	""" Testing apparatus for realm of reckoning """
 	suite = TestSuite.TestSuite()
-	print "\n-----Testing Character-----"
-	test_char = Character('male', 'Rick', 10, 5, 5, 0, [], [0,0])
-	print "character:", test_char
-	print "\nTesting set_coords get_coords and get_coord..."
+	print("\n-----Testing Character-----")
+	test_char = Character('male', 'Rick', 10, 5, 5, 0, [], [0,0], '')
+	print("character:", test_char)
+	print("\nTesting set_coords get_coords and get_coord...")
 	suite.run_test(test_char.get_coords(), [0,0], "Test 1:")
 	test_char.set_coords([1,1])
 	suite.run_test(test_char.get_coords(), [1,1], "Test 2:")
@@ -19,15 +19,15 @@ def realm_test():
 	suite.run_test(test_char.get_coord(0), 5, "Test 3:")
 	test_char.set_coords([1,2])
 	suite.run_test(test_char.get_coord(1), 2, "Test 4:")
-	print "\nTesting attrib_list (check visually)..."
+	print("\nTesting attrib_list (check visually)...")
 	suite.run_test(test_char.attrib_list(), None, "Test 5:")
-	print "\nTesting add_to_inventory and get_inventory (check visually)..."
-	print "Adding 'flint' and 'eggs' to inventory ..."
+	print("\nTesting add_to_inventory and get_inventory (check visually)...")
+	print("Adding 'flint' and 'eggs' to inventory ...")
 	suite.run_test(test_char.add_to_inventory('flint'), True, "Test 6:")
 	suite.run_test(test_char.add_to_inventory('eggs'), True, "Test 7:")
 	suite.run_test(test_char.get_inventory(), ['flint', 'eggs'], "Test 8:")
 	
-	print"\n"
+	print("\n")
 	suite.report_results()
 
-realmTest()
+realm_test()
